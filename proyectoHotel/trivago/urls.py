11f7252b63350ee87b566e50abecdcf643c063f2
AdrianCxn <adn.bahrein@gmail.com>
2025-09-26 13:03:05 -0600

    Crear vistas, urls y templates de todas las páginas

--- proyectoHotel/trivago/urls.py
+++ proyectoHotel/trivago/urls.py
@@ -2,7 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='main'),
+    path('', views.index, name='index'),
+    # Paths para habitaciones
+    path('tipohabitacion/', views.tipohabitacion, name='tipohabitacion'),
+    path('habitaciones/', views.habitaciones, name='habitaciones'),
+    # Paths para administracion
+    path('staff/', views.staff, name='staff'),
+    path('descuentos/', views.descuentos, name='descuentos'),
+    path('huespedes/', views.huespedes, name='huespedes'),
+    path('reservas/', views.reservas, name='reservas'),
+    path('consumos/', views.consumos, name='consumos'),
+    # Path para restaurante
     path('restaurante/', views.restaurante, name='restaurante'),
-    path('tipohabitacion/', views.tipohabitacion, name='tipohabitacion')
+    # Paths para productos
+    path('productos/', views.productos, name='productos'),
+    path('inventario/', views.inventario, name='inventario'),
+    path('distribuidores/', views.distribuidores, name='distribuidores')
 ]
--- proyectoHotel/trivago/views.py
+++ proyectoHotel/trivago/views.py
@@ -4,14 +4,55 @@ from django.shortcuts import render
 from .models import Tipos
 
 # Create your views here.
-def main(request):
+# Vista para index
+def index(request):
     tipos = Tipos.objects.all()
     return render(request, "trivago/index.html")
 
 
+# Vistas para habitaciones
+def tipohabitacion(request):
+    return render(request, "trivago/tipohabitacion.html")
+
+
+def habitaciones(request):
+    return render(request, "trivago/habitaciones.html")
+
+
+# Vistas para administracion
+def staff(request):
+    return render(request, "trivago/staff.html")
+
+
+def descuentos(request):
+    return render(request, "trivago/descuentos.html")
+
+
+def huespedes(request):
+    return render(request, "trivago/huespedes.html")
+
+
+def reservas(request):
+    return render(request, "trivago/reservas.html")
+
+
+def consumos(request):
+    return render(request, "trivago/consumos.html")
+
+
+# Vista para restaurante
 def restaurante(request):
     return render(request, "trivago/restaurante.html")
 
 
-def tipohabitacion(request):
-    return render(request, "trivago/tipohabitacion.html")
+# Vistas para productos
+def productos(request):
+    return render(request, "trivago/productos.html")
+
+
+def inventario(request):
+    return render(request, "trivago/inventario.html")
+
+
+def distribuidores(request):
+    return render(request, "trivago/distribuidores.html")
