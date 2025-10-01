7bf904ddddcec9e84bbe6581b665a27eeb62bb8c
Andrea :P <andreaf.q2909@gmail.com>
2025-09-30 22:29:11 -0600

    Descuentos y tipos habitacion
    Se agregó la vista de tipos, y el crud completo de descuentos

--- proyectoHotel/trivago/urls.py
+++ proyectoHotel/trivago/urls.py
@@ -5,13 +5,19 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Paths para habitaciones
     path('tipohabitacion/', views.tipohabitacion, name='tipohabitacion'),
+
     path('habitaciones/', views.habitaciones, name='habitaciones'),
     path('agregar_habitacion/', views.agregar_habitacion, name='agregar_habitacion'),
     path('editar_habitacion/<int:id>/', views.editar_habitacion, name='editar_habitacion'),
+
     path('eliminar_habitacion/<int:id>/', views.eliminar_habitacion, name='eliminar_habitacion'),
     # Paths para administracion
     path('staff/', views.staff, name='staff'),
     path('descuentos/', views.descuentos, name='descuentos'),
+    path('descuentos/agregar/', views.agregar_descuento, name='agregar_descuento'),
+    path('descuentos/editar/<int:id>/', views.editar_descuento, name='editar_descuento'),
+    path('descuentos/eliminar/<int:id>/', views.eliminar_descuento, name='eliminar_descuento'),
+
     path('huespedes/', views.huespedes, name='huespedes'),
     path('reservas/', views.reservas, name='reservas'),
     path('consumos/', views.consumos, name='consumos'),
--- proyectoHotel/trivago/views.py
+++ proyectoHotel/trivago/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render, redirect
 from django.contrib import messages
 from django.db import IntegrityError
-from .models import Habitaciones, Huespedes, Reservas, Tipos
+from .models import Habitaciones, Huespedes, Reservas, Tipos, Descuentos
 
 # Create your views here.
 # Vista para index
@@ -12,7 +12,10 @@ def index(request):
 
 # Vistas para habitaciones
 def tipohabitacion(request):
-    return render(request, "trivago/tipohabitacion.html")
+    tipos = Tipos.objects.all()
+    return render(request, "trivago/tipohabitacion.html", {
+        "tipos": tipos
+    })
 
 def habitaciones(request):
     habitaciones = Habitaciones.objects.all()
@@ -95,9 +98,70 @@ def eliminar_habitacion(request, id):
 def staff(request):
     return render(request, "trivago/staff.html")
 
-
 def descuentos(request):
-    return render(request, "trivago/descuentos.html")
+    descuentos = Descuentos.objects.all()
+    return render(request, "trivago/descuentos.html", {
+        "descuentos": descuentos
+    })
+
+def agregar_descuento(request):
+    descuentos = Descuentos.objects.all()
+    if request.method == "POST":
+        fecha_inicio = request.POST.get("fecha_inicio")
+        fecha_fin = request.POST.get("fecha_fin")
+        porcentaje = request.POST.get("descuento")
+
+        try:
+            nuevo_descuento = Descuentos(
+                fecha_inicio=fecha_inicio,
+                fecha_fin=fecha_fin,
+                descuento=porcentaje
+            )
+            nuevo_descuento.save()
+            messages.success(request, "Descuento agregado exitosamente.")
+            return redirect('descuentos') 
+        except IntegrityError:
+            messages.error(request, "Error: Ya existe un descuento con estos datos.")
+        except Exception as e:
+            messages.error(request, f"Error al agregar el descuento: {e}")
+
+    return render(request, "trivago/agregar_descuento.html")
+
+def editar_descuento(request, id):
+    descuento = get_object_or_404(Descuentos, pk=id)
+
+    if request.method == "POST":
+        descuento.fecha_inicio = request.POST.get('fecha_inicio')
+        descuento.fecha_fin = request.POST.get('fecha_fin')
+        descuento.descuento = request.POST.get('descuento')
+
+        try:
+            descuento.save()
+            messages.success(request, "Descuento actualizado exitosamente.")
+            return redirect('descuentos')  # redirige a la lista de descuentos
+        except IntegrityError:
+            messages.error(request, "Error: Ya existe un descuento con estos datos.")
+        except Exception as e:
+            messages.error(request, f"Error al actualizar el descuento: {e}")
+
+    return render(request, "trivago/editar_descuento.html", {
+        "descuento": descuento
+    })
+
+def eliminar_descuento(request, id):
+    descuentos = get_object_or_404(Descuentos, pk=id)
+
+    if request.method == "POST":
+        try:
+            descuentos.delete()
+            messages.success(request, "Descuento eliminado exitosamente.")
+        except Exception as e:
+            messages.error(request, f"Error al eliminar el descuento: {e}")
+        return redirect("descuentos")
+    else:
+        return redirect("descuentos")
+    
+
 
 
 def huespedes(request):
