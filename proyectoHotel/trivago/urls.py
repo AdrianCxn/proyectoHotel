88e128c0c60c327b37dede88a68d441ceb0943b4
AdrianCxn <adn.bahrein@gmail.com>
2025-10-01 00:55:31 -0600

    Add staff management views and templates
    Implemented staff CRUD operations: added views for creating, editing, and deleting staff members, and updated URLs accordingly. Added and enhanced templates for staff management, including feedback messages and improved UI. Also updated staff listing to support edit and delete actions.

--- proyectoHotel/trivago/urls.py
+++ proyectoHotel/trivago/urls.py
@@ -11,11 +11,14 @@ urlpatterns = [
     path('habitaciones/', views.habitaciones, name='habitaciones'),
     path('agregar_habitacion/', views.agregar_habitacion, name='agregar_habitacion'),
     path('editar_habitacion/<int:id>/', views.editar_habitacion, name='editar_habitacion'),
-
     path('eliminar_habitacion/<int:id>/', views.eliminar_habitacion, name='eliminar_habitacion'),
 
     # Paths para administracion
     path('staff/', views.staff, name='staff'),
+    path('agregar_staff/', views.agregar_staff, name='agregar_staff'),
+    path('editar_staff/<int:id>/', views.editar_staff, name='editar_staff'),
+    path('eliminar_staff/<int:id>/', views.eliminar_staff, name='eliminar_staff'),
+
     path('descuentos/', views.descuentos, name='descuentos'),
     path('descuentos/agregar/', views.agregar_descuento, name='agregar_descuento'),
     path('descuentos/editar/<int:id>/', views.editar_descuento, name='editar_descuento'),
